cbf6e6273c80f9cd2986ec5ed573d77063607022
Yatt1n <mateoyatin@hotmail.com>
2024-10-28 09:23:25 -0300

    catalogo actualizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { LoginComponent } from './login/login/login.component';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CommonModule } from '@angular/common';
+import { CatalogComponent } from './catalog/catalog.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: `./app.component.html`,
-  imports: [RouterOutlet, LoginComponent, RegisterComponent, HomeComponent, CommonModule],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent, HomeComponent, CommonModule, CatalogComponent],
 })
 export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
 
 
 export const routes: Routes = [
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'catalog', component: CatalogComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
